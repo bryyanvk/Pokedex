28e6670ae50617ac69147b4ab9fe317915b04266
Bryyan <bryangam3r2019@gmail.com>
2026-04-10 13:03:53 -0300

    feat: :sparkles: final
    finalizei colocando os pokemons e o linear

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -8,7 +8,7 @@ export function Welcome() {
       
        
          
-            <div className="text-2xl font-bold text-white bg-red-500 justify-between flex items-center p-4">
+            <div className="text-2xl font-bold text-white bg-linear-to-r from-red-500 to-yellow-500 justify-between flex items-center p-4">
               <div>
                 <h1>Pokedex </h1>
 
@@ -24,48 +24,58 @@ export function Welcome() {
           
   
           
-      <div className=" flex flex-col justify-between min-h-[calc(106vh-106px)]"> 
-        <div className="flex  justify-between gap-2 p-2 ">
-          <input className=" border-2 border-gray-300 rounded-lg p-2 mt-4 bg-white  w-300" placeholder=""  />
-          <button className="border-red-200 text-red-200 hover:border-transparent hover:bg-red-700 active:bg-red-700 bg-red-500 rounded-lg p-2 mt-4  ">
-            Buscar
-          </button>
+      <div className=" flex flex-col justify-between min-h-[calc(104.4vh-106px)] bg-red-100"> 
+        <div className="flex h-20 items-center bg-white mx-10 mt-10 rounded-lg p-4">
+          <form className="w-full relative">
+             <input 
+                type="text"
+                className="w-330 p-3 pb- pb-1 border-b border-gray-200 focus:outline-none "
+               />
+             <button className="absolute right-2 bottom-1 bg-red-500 border-red-200 text-red-200 hover:border-transparent hover:bg-red-700 active:bg-red-700 bg-red-500 rounded-lg p-2 mt-10  ">
+               Buscar
+             </button>
+          </form>
+        
           </div>
 
       
          
-           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 p-3 ">
+           <div className="flex flex-wrap gap-8 justify-center mt-10 ">
   
-            <div className=" text-center sm:text-left bg-yellow-200  rounded-lg p-6 "> 
+            <div className="flex flex-col h-70 w-110 items-center justify-center text-center sm:text-center bg-yellow-200  rounded-2xl p-6 "> 
                <div className="space-y-0.5">
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="Pikachu" className="w-40 h-40 mx-auto mb-2" />
                   <p className="text-lg font-semibold text-black">Pikachu</p>
                </div>
             <button className="border-purple-200 text-yellow-200 hover:border-transparent hover:bg-yellow-600 bg-yellow-500 rounded-lg p-2 mt-4  ">
               Eletrico
             </button>
             </div>
-            <div className="space-y-2 text-center sm:text-left bg-green-300 p-6 rounded-lg">
+            <div className="flex flex-col h-70 w-110 items-center justify-center text-center sm:text-center bg-green-300 p-6 rounded-lg">
                 <div className="space-y-0.5">
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Bulbasaur" className="w-40 h-40 mx-auto mb-2" />
                     <p className="text-lg font-semibold text-black">Bulbasaur</p>
                 </div>
-              <button className="border-green-200 text-green-300 hover:border-transparent hover:bg-green-700 hover:text-black   active:bg-green-700 bg-green-600 rounded-lgp-2 mt-4 ">
+              <button className="border-green-200 text-green-300 hover:border-transparent hover:bg-green-700 bg-green-500 rounded-lg p-2 mt-4  ">
                 Planta
               </button>
               </div>
-            <div className="space-y-2 text-center sm:text-left bg-red-300 p-6 rounded-lg">
+            <div className="flex flex-col h-70 w-110 items-center justify-center text-center sm:text-center bg-red-300 p-6 rounded-lg">
                 <div className="space-y-0.5">
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" alt="Charmander" className="w-40 h-40 mx-auto mb-2" />
                     <p className="text-lg font-semibold text-black">Charmander</p>
                 </div>
-              <button className="border-red-200 text-red-200 hover:border-transparent hover:bg-red-700 hover:text-black   active:bg-red-700 bg-red-500">
+              <button className="border-red-200 text-red-200 hover:border-transparent hover:bg-red-700 bg-red-500 rounded-lg p-2 mt-4  ">
                 Fogo
               </button>
               </div>  
   
-              <div className="space-y-2 text-center sm:text-left bg-blue-200 p-6 rounded-lg">
+              <div className="flex flex-col h-70 w-110 items-center justify-center text-center sm:text-center bg-blue-200 p-6 rounded-lg">
                   <div className="space-y-0.5">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="Squirtle" className="w-40 h-40 mx-auto mb-2" />
                       <p className="text-lg font-semibold text-black bg-">Squirtle</p>
                   </div>
-                <button className="border-blue-200 text-blue-200 hover:border-transparent hover:bg-blue-700 hover:text-black   active:bg-blue-700 bg-blue-600">
+                <button className="border-blue-200 text-blue-200   hover:border-transparent hover:bg-blue-700 bg-blue-500 rounded-lg p-2 mt-4  ">
                   Agua
                 </button>
               </div>
